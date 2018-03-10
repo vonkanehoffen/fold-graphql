b9aa6be635f5cced9acd36fc08fc88f3958f108f
Kane Clover <kane@assure24.com>
2018-03-10 15:06:10 +0000

    Basic downshift autocomplete

--- frontend/src/containers/main/NavBar.js
+++ frontend/src/containers/main/NavBar.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import RaisedButton from 'material-ui/RaisedButton'
+import SearchBox from '../../components/SearchBox'
 
 const NavBar = ({ session, doSignOut }) => {
   return (
@@ -14,6 +15,10 @@ const NavBar = ({ session, doSignOut }) => {
         <div>
           <span>User: {session.idToken.payload.name}</span>
           <RaisedButton label="Sign out" onClick={doSignOut}/>
+          <SearchBox
+            tags={['bear', 'badger', 'donkey', 'ocelot', 'dog', 'cat', 'emu']}
+            onChange={(selection => console.log(selection))}
+          />
         </div>
         :
         <div>Not logged in</div>
